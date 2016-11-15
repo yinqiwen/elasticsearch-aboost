06ab7dd917cbc27c755f43ac253745dfbf947263
yinqiwen <yinqiwen@gmail.com>
2016-11-15 23:00:31 +0800

    update

--- src/main/java/org/yinqiwen/elasticsearch/plugin/aboost/ABoostPlugin.java
+++ src/main/java/org/yinqiwen/elasticsearch/plugin/aboost/ABoostPlugin.java
@@ -1,33 +1,51 @@
 package org.yinqiwen.elasticsearch.plugin.aboost;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-
-/**
- * Hello world!
- *
- */
-
+import org.elasticsearch.common.inject.AbstractModule;
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.common.inject.multibindings.Multibinder;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.SearchPlugin;
+import org.yinqiwen.elasticsearch.plugin.aboost.cfg.Config;
 import org.yinqiwen.elasticsearch.plugin.aboost.functions.FieldMatchFactorFunctionBuilder;
 import org.yinqiwen.elasticsearch.plugin.aboost.functions.FieldRefValueFactorFunctionBuilder;
-public class ABoostPlugin extends Plugin  implements SearchPlugin
-{
+
+public class ABoostPlugin extends Plugin implements SearchPlugin {
 	public static String PLUGIN_NAME = "aboost";
-	
+
 	@Override
-	public  List<ScoreFunctionSpec<?>> getScoreFunctions() {
+	public Collection<Module> createGuiceModules() {
+		return Collections.<Module> singletonList(new ConfiguredModule());
+	}
+
+	@Override
+	public List<ScoreFunctionSpec<?>> getScoreFunctions() {
 		// TODO Auto-generated method stub
 		List<ScoreFunctionSpec<?>> funcs = new ArrayList<>();
-		ScoreFunctionSpec<FieldRefValueFactorFunctionBuilder> s1 = new ScoreFunctionSpec<FieldRefValueFactorFunctionBuilder>(FieldRefValueFactorFunctionBuilder.NAME, FieldRefValueFactorFunctionBuilder::new, FieldRefValueFactorFunctionBuilder::fromXContent);
-		ScoreFunctionSpec<FieldMatchFactorFunctionBuilder> s2 = new ScoreFunctionSpec<FieldMatchFactorFunctionBuilder>(FieldMatchFactorFunctionBuilder.NAME, FieldMatchFactorFunctionBuilder::new, FieldMatchFactorFunctionBuilder::fromXContent);
+		ScoreFunctionSpec<FieldRefValueFactorFunctionBuilder> s1 = new ScoreFunctionSpec<FieldRefValueFactorFunctionBuilder>(
+				FieldRefValueFactorFunctionBuilder.NAME, FieldRefValueFactorFunctionBuilder::new,
+				FieldRefValueFactorFunctionBuilder::fromXContent);
+		ScoreFunctionSpec<FieldMatchFactorFunctionBuilder> s2 = new ScoreFunctionSpec<FieldMatchFactorFunctionBuilder>(
+				FieldMatchFactorFunctionBuilder.NAME, FieldMatchFactorFunctionBuilder::new,
+				FieldMatchFactorFunctionBuilder::fromXContent);
 		funcs.add(s1);
 		funcs.add(s2);
 		return funcs;
 	}
-	
 
+	public static class ConfiguredModule extends AbstractModule {
+		@Override
+		protected void configure() {
+			bind(Config.class).asEagerSingleton();
+//			Multibinder<Config> multibinder = Multibinder.newSetBinder(binder(), Config.class);
+//			multibinder.addBinding().to
+//			multibinder.addBinding().toInstance(new Twix());
+//			multibinder.addBinding().toProvider(SnickersProvider.class);
+//			multibinder.addBinding().to(Skittles.class);
+		}
+	}
 }
--- src/main/java/org/yinqiwen/elasticsearch/plugin/aboost/cfg/Config.java
+++ src/main/java/org/yinqiwen/elasticsearch/plugin/aboost/cfg/Config.java
@@ -4,11 +4,14 @@
 package org.yinqiwen.elasticsearch.plugin.aboost.cfg;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Map;
 
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.io.PathUtils;
+import org.elasticsearch.common.logging.ESLoggerFactory;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.yinqiwen.elasticsearch.plugin.aboost.ABoostPlugin;
@@ -18,20 +21,24 @@ import org.yinqiwen.elasticsearch.plugin.aboost.ABoostPlugin;
  *
  */
 public class Config {
+	private static final Logger logger = ESLoggerFactory.getLogger(Config.class.getName());
 	private Environment environment;
 	private Settings settings;
 	private Map<String, Double> factorValueRef;
 
 	@Inject
-	public Config(Environment env,Settings settings) {
-		this.environment = env;
-		this.settings=settings;
+	public Config(Environment env) throws IOException {
+		Path path = env.pluginsFile().resolve("aboost/config/aboost.json");
+		settings = Settings.builder().loadFromPath(path).build();
+		this.environment = env;;
+		
 
 //		this.useSmart = settings.get("use_smart", "false").equals("true");
 //		this.enableLowercase = settings.get("enable_lowercase", "true").equals("true");
 //		this.enableRemoteDict = settings.get("enable_remote_dict", "true").equals("true");
 
 		//Dictionary.initial(this);
+		logger.info("####" + settings.get("dicts", "false"));
 
 	}
 
